ec06e101aede7f05940e9e78924730d64df9bb2b
riyaa-aa <riya.maiya22@gmail.com>
2023-09-13 17:13:47 -0400

    clean up

--- tasks.py
+++ tasks.py
@@ -10,14 +10,6 @@ from python_tsp.exact import solve_tsp_dynamic_programming
 f = open('data/coords.json')
 data = json.load(f)
 
-for i in data['waypoints']:
-    # print(i)
-    # print(i['latitude'])
-    '''
-    for x in i:
-        print(x)
-    '''
-
 # task 2
 
 def find_distance(wp1, wp2):
@@ -38,19 +30,13 @@ def find_distance(wp1, wp2):
 waypoint1 = data['waypoints'][0]
 waypoint2 = data['waypoints'][1]
 
-# print(find_distance(waypoint1, waypoint2)) 
+# find_distance(waypoint1, waypoint2)
 # output: 1350.9321704989154 which is correct!
-
-# print(len(data['waypoints']))
 
 # task 3
 
 dist_arr = [[0 for i in range(14)] for j in range(14)] # initializes a 2d array that is 14x14, where each element = 0
 
-'''
-for row in data['waypoints']:
-    for col in data['waypoints']:
-'''
 for i in range(14):
     waypointi = data['waypoints'][i]
     for x in range(14):
@@ -58,14 +44,9 @@ for i in range(14):
         dist_arr[i][x] = find_distance(waypointi,waypointx) # filling up the arr row by row
 
 # checking test case again; should give the distance between waypoint 1 and 2, which should be 1350.9321704989154
-# print(dist_arr[0][1])
+# dist_arr[0][1]
 # output: 1350.9321704989154 -- correct!
-
-# for row in dist_arr:
-#     print(row+'\n')
-# print(np.matrix(dist_arr))
-# this looks right???? seems like every [n,n] element is 0, which is at least definitely correct 
-# (distances between waypoint n and waypoint n are always zero since they are at the same point)
+# also: dist_arr[i][x] where i == x is always 0, which is correct since distances between waypoint n and waypoint n are always zero since they are at the same point
 
 # hamiltonian cycle: every node visited once, come back to the starting node
 # minimize the total cost of this cycle; output the minimum weight hamiltonian cycle
@@ -84,14 +65,14 @@ memo = [[(-1, [])]*(1 << (n)) for _ in range(n)] # list of lists of -1s + empty 
 def get_min_dist(i,mask):
 
     if mask == ((1 << i)): 
-    # implies all the bits but 1 have been visited
+    # implies all the bits but 1 (the endpoint) have been visited
         return dist_arr[0][i], [0,i] # the distance from waypoint 0 to waypoint i bc all other waypoints have been visited
     
     if memo[i][mask][0] != -1: # min dist has been recorded
-        return memo[i][mask]
+        return memo[i][mask] # this is the dynamic programming part that prevents the algorithm from having to repeat calculations
 
     # this is just setting a max value that the total dist could be
-    # from the dist_arr, I know that most distances are 3-4 digits, and there are 14 digits
+    # from the dist_arr, I know that the distances are 3-4 digits, and there are 14 distances
     # assuming maximum, I just multiplied the largest 4 digit number by 14
     min_dist = 9999*14 
     shortest_route = [] # to store the path that is the shortest
@@ -104,8 +85,8 @@ def get_min_dist(i,mask):
         if (mask & (1 << j)) != 0 and j != i and j != 0:
             md, route = get_min_dist(j, mask & (~(1 << i))) 
             # this is where the mask is changing
-            # making j i, so traversing from 0 to n now
             # flipping the ith node to 0 on the mask
+            # saying that that node is now being visited
             if min_dist > md+dist_arr[j][i]:
                 min_dist = md+dist_arr[j][i]
                 shortest_route = route + [i]
@@ -114,33 +95,9 @@ def get_min_dist(i,mask):
 
     return min_dist, shortest_route
 
-'''
-ans = 9999*14 
-shortest_route = []
-
-for i in range(0, n):
-    # try to go from node 0 visiting all nodes in between to i
-    # then return from i taking the shortest route to 0
-    min_d, route = get_min_dist(i, (1 << (n))-1)
-    if ans > min_d + dist_arr[i][0]:
-        ans = min_d + dist_arr[i][0]
-        shortest_route = route 
-
-shortest_route.reverse() 
-'''
-
-min_d, route = get_min_dist(0, (1 << (n))-1)
+min_d, route = get_min_dist(0, (1 << (n))-1) # setting mask to be 14 1s; every node needs to be visited
 route.reverse() # because we got the min dist by going from the end to the beginning, the route we got needs to be reversed
-# doing memo[0][16383] will give you the min dist + route; node 0 is the endpoint, 16363 = binary list of 14 1s, implying all nodes have been visited
-# print(memo[0][16383])
-'''
-for i in range(0,13):
-    for x in range((1<<14)-1):
-        if memo[i][x][0] != -1:
-            print(memo[i][x])
-            print("node: " + str(i))
-            print("combo: " + str(x))
-'''
+# memo[0][16383] will give you the min dist + route; node 0 is the endpoint, 16383 = binary list of 14 1s, meaning that the min dist value stored in memo[0][16383] takes into account every single node
 
 print("The minimum distance to travel to all 14 waypoints = " + str(min_d))
 # output: 13358.79556824653
@@ -150,10 +107,8 @@ print("The shortest route to take = " + str(route))
 # using a python library to check if my code outputs the correct answer
 distance_matrix = np.array(dist_arr)
 permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
-print("permutation: " + str(permutation))
-# output: [0, 13, 4, 5, 6, 7, 8, 9, 10, 11, 12, 3, 2, 1]
-print("distance: " + str(distance))
-# output: 13358.79556824653
+# permutation output: [0, 13, 4, 5, 6, 7, 8, 9, 10, 11, 12, 3, 2, 1]
+# distance output: 13358.79556824653
 # code is correct!
 
 # task 4
@@ -168,22 +123,17 @@ for i in data['flyZones']:
         if minLong > (x['longitude']):
             minLong = (x['longitude'])
 
-# print(minLat, minLong)
-
 def get_coords(waypoint, min_lat, min_long):
 
     wp_lat = list(waypoint.values())[0] # latitude of waypoint
     wp_long = list(waypoint.values())[1] # longitude of waypoint
 
-    # x = math.dist(min_lat,wp_long)
-    # y = math.dist(min_long,wp_lat)
-    x = abs(wp_long - min_long)
-    y = abs(wp_lat - min_lat)
-    # x = wp_long
-    # y = wp_lat
+    x = abs(wp_long - min_long) # normalizing the waypoint longitude
+    y = abs(wp_lat - min_lat) # normalizing the waypoint latitude
 
     return x,y
 
+# to show direction that the plane is travelling in
 def draw_arrows(xarr, yarr):
     x0 = xarr[i]
     x1 = xarr[i+1]
@@ -198,22 +148,23 @@ def draw_arrows(xarr, yarr):
 xcoordsArr = []
 ycoordsArr = []
 
+# getting array of points to plot
 for i in route:
     x,y = get_coords(data['waypoints'][i], minLat, minLong)
     xcoordsArr.append(x)
     ycoordsArr.append(y)
 
-plt.scatter(xcoordsArr,ycoordsArr)
-plt.plot(xcoordsArr,ycoordsArr)
+plt.scatter(xcoordsArr,ycoordsArr) # plotting points
+plt.plot(xcoordsArr,ycoordsArr) # plotting line to join points
 plt.xlabel('longitude')
 plt.ylabel('latitude')
 plt.grid('equal')
-plt.text(xcoordsArr[0]-4e-4,ycoordsArr[0]+4e-4, "start")
+plt.text(xcoordsArr[0]-4e-4,ycoordsArr[0]+4e-4, "start") # where you start; should be waypoint 0
 plt.title('shortest route')
 for i in range(len(xcoordsArr)-1):
     xpos, ypos, xdir, ydir = draw_arrows(xcoordsArr,ycoordsArr)
-    plt.annotate("", xytext=(xpos,ypos),xy=(xpos+0.001*xdir,ypos+0.001*ydir), 
-    arrowprops=dict(arrowstyle="->", color='k'), size = 20)
-#plt.show()
+    plt.annotate("", xytext=(xpos,ypos),xy=(xpos+0.001*xdir,ypos+0.001*ydir), # putting arrows along the line; direction the plane moves in
+    arrowprops=dict(arrowstyle="->", color='k'), size = 20) # styling the arrows
+plt.show()
 
 f.close()
